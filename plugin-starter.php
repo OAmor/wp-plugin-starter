97b0c439e3c201e850566162a3319bfd9bded26c
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-11-12 08:33:41 +0100

    add securit ABS test

--- plugin-starter.php
+++ plugin-starter.php
@@ -10,6 +10,10 @@ Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
 
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 class plugin_starter{
     public function __construct(){
         include_once plugin_dir_path(__FILE__).'inc/pageScripts.php';
