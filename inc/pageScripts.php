267c7136111c64acb43d6120611d23577823ee5a
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-11-12 08:33:05 +0100

    fix directory conflits

--- inc/pageScripts.php
+++ inc/pageScripts.php
@@ -2,11 +2,13 @@
 
 class pageScripts{
     public function __construct(){
-        add_action('wp_enqueue_scripts',array($this,'add_scripts'));
+        /* Load Scripts And Styles */
+        add_action('wp_enqueue_scripts',array($this,'add_scripts'),999);
     }
 
     public function add_scripts(){
-        wp_enqueue_script('test-script',get_template_directory_uri().'/plugins/plugin-starter/scripts/main.js');
-        wp_enqueue_style('test-style',get_template_directory_uri().'/plugins/plugin-starter/styles/main.css');
+     	wp_enqueue_style('test-style',plugin_dir_url(__FILE__).'styles/main.css');
+        wp_enqueue_script('test-script', plugin_dir_url(__FILE__).'scripts/main.js', array(), '1.0.0', true);
+        
     }
 }
